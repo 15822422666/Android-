5afa443e8b4c16c80dcefd6eea6c41f0fe6710bf
PussyGirl <55914926+15822422666@users.noreply.github.com>
2020-02-23 17:03:34 +0800

    第一次提交

---
+++ app/src/main/java/com/example/myapplication/MainFragment.java
@@ -0,0 +1,51 @@
+package com.example.myapplication;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class MainFragment extends Fragment {
+
+    TextView textView;
+    Context context;
+    ListView listView;
+
+    public MainFragment() {
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        context = getActivity();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        textView = new TextView(context);
+        textView.setGravity(Gravity.CENTER);
+        textView.setTextSize(25);
+        textView.setTextColor(Color.RED);
+        return textView;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        textView.setText("asdf");
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/fragment.java
@@ -0,0 +1,49 @@
+package com.example.myapplication;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+public class fragment extends Fragment {
+
+    TextView textView;
+    Context context;
+    ProgressBar progressBar;
+
+    public fragment() {
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        context = getActivity();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        textView = new TextView(context);
+        textView.setGravity(Gravity.CENTER);
+        textView.setTextSize(25);
+        textView.setTextColor(Color.RED);
+        return textView;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        textView.setText("sadfasdfasd");
+    }
+}
